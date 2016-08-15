cce9078ea1ce60b2e731c681fbb237297bb92c5b
anderson.santos <anderson.santos@pcinformatica.com.br>
2016-08-15 17:37:27 -0300

    minor adjusts to fix release

---
+++ dist/StackPanelPlugin.js
@@ -0,0 +1,130 @@
+/**
+ * Created by anderson.santos on 05/08/2016.
+ */
+rz.plugins.StackPanelPlugin = function(params){
+    var helpers = {
+        defaultitemRenderer:{
+            render: function(data,sender){
+                var sb = new StringBuilder();
+                sb.appendFormat('<div id="{0}_item_{1}" class="{3}">{2}</div>',
+                    sender.params.baseID,
+                    generateRandomID(),
+                    data,
+                    sender.params.stackItemClass
+                );
+                return sb.toString();
+            },
+            initialize:undefined
+        }
+        ,
+        defaultEventHandlers: {
+            afterRender:undefined,
+            beforeRender:undefined,
+            buildingContent:undefined
+        }
+    };
+    var $this = this;
+    var initialize = function(){
+        var defaultParams = {
+            orientation:"vertical",
+            verticalContainerStyle:"ui vertical menu",
+            horizontalContainerStyle:"ui menu",
+            target:"div .rutezangada-stack-panel-container",
+            baseID:generateRandomID(),
+            itemRenderer: helpers.defaultitemRenderer,
+            eventHandlers: helpers.defaultEventHandlers,
+            stackItemClass: "draggable item",
+            sortParams:{
+                containerSelector: "div.menu",
+                itemSelector: "div.item",
+                placeholder: '<div class="placeholder"></div>',
+                delay: 300,
+                vertical: true
+            }
+        };
+        $this.params = $.extend(true, {}, defaultParams, params);
+        render();
+    };
+
+    var resolveContainerClass = function(){
+        var params = $this.params;
+        return params.orientation=="vertical"?
+            params.verticalContainerStyle:
+            params.horizontalContainerStyle;
+    };
+    var raiseEvent = function(event,args){
+        if($this.params.eventHandlers[event]) $this.params.eventHandlers[event]($this,args);
+    };
+    
+    var render = function(){
+        var params = $this.params;
+        var sb = new StringBuilder();
+        raiseEvent("beforeRender");
+        sb.appendFormat('<div id="{0}" class="{1}">',params.baseID,resolveContainerClass());
+        raiseEvent("buildingContent");
+        sb.appendFormat('</div>');
+        $(params.target).html(sb.toString());
+
+        var p = params.sortParams;
+        p.vertical = (params.orientation=="vertical");
+
+        $("#" + params.baseID).sortable(p);
+        raiseEvent("afterRender");
+
+    };
+
+    var resolveItemRenderer = function(r){
+        if(r===undefined){
+            return  $this.params.itemRenderer;
+        }
+        else{
+            if(typeof(r)=="string"){
+                //return registered-items-renderer[r]
+                throw "not implemented yet"
+            }
+            else{
+                return r;
+            }
+        }
+    };
+
+    var plotItem = function(el, pos){
+        if(pos===undefined){
+            el.appendTo("#" + $this.params.baseID);
+        }
+        else{
+            var refItem = $("#" + $this.params.baseID).children()[pos];
+            if(refItem !==undefined){
+                el.insertBefore(refItem);
+            }
+            else{
+                el.appendTo("#" + $this.params.baseID);
+            }
+        }
+    };
+
+    this.addItem = function(itemData,customrender){
+        var itemRenderer = resolveItemRenderer(customrender);
+        var html = itemRenderer.render(itemData,$this);
+        var el = $(html);
+        plotItem(el);
+        if(itemRenderer.initialize) itemRenderer.initialize(itemData,$this,el);
+    };
+
+    this.insertItem = function(itemData,pos,customrender){
+        var itemRenderer = resolveItemRenderer(customrender);
+        var html = itemRenderer.render(itemData,$this);
+        var el = $(html);
+        plotItem(el,pos);
+        if(itemRenderer.initialize) itemRenderer.initialize(itemData,$this,el);
+    };
+
+    this.removeItem = function(selector){
+        $("#" + $this.params.baseID).children(selector).detach();
+    };
+
+    this.removeItemAt = function(pos){
+        $($("#" + $this.params.baseID).children()[pos]).detach();
+    };
+    initialize();
+};
--- gruntfile.js
+++ gruntfile.js
@@ -5,15 +5,13 @@ module.exports = function (grunt) {
     grunt.initConfig({
             options: {
                 srcFiles: [
-                    "src/NamespaceDeclares.js",
-                    "src/RuteZangada.js",
-                    "src/WidgetEngine.js"
+                    "src/StackPanelPlugin.js"
                 ]
             },
             concat: {
                 dist: {
                     src: ['<%= options.srcFiles %>'],
-                    dest: "dist/RZClientEngine.js"
+                    dest: "dist/StackPanelPlugin.js"
                 }
             },
             uglify: {
@@ -22,31 +20,8 @@ module.exports = function (grunt) {
                 },
                 my_target: {
                     files: {
-                        "dist/RZClientEngine.min.js": ['dist/RZClientEngine.js']
+                        "dist/StackPanelPlugin.min.js": ['dist/StackPanelPlugin.js']
                     }
-                }
-            },
-            copy: {
-                test: {
-                    src: 'dist/RZClientEngine.js',
-                    dest: 'test/src/RZClientEngine.js'
-                },
-                test_template: {
-                    options: {
-                        processContent: function (content) {
-                            return grunt.template.process(content);
-                        }
-                    },
-                    src: 'test/template/SpecRunner.html.ejs',
-                    dest: 'test/SpecRunner.html'
-                }
-            },
-            jasmine: {
-                src: 'test/src/*.js',
-                options: {
-                    specs: 'test/spec/*.spec.js',
-                    helpers: 'test/specs/helpers/*.js',
-                    vendor:['test/lib/*.js']
                 }
             }
         }
@@ -54,8 +29,7 @@ module.exports = function (grunt) {
 // Plugins do Grunt
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
 
 
-    grunt.registerTask('default', ['concat', 'uglify', 'copy:test', 'copy:test_template']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 };
